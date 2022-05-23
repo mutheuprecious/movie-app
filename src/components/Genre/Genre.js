db5e09cedaa7cca51f5c22da845eb3edc072aca7
Precious <preciousmaregwa1997@gmail.com>
2022-05-23 20:30:49 +0300

    add genre

--- src/components/Genre/Genre.js
+++ src/components/Genre/Genre.js
@@ -1,70 +1,94 @@
-import { Chip } from "@material-ui/core";
 import axios from "axios";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./Genres.css";
 
-const Genres = ({
-  selectedGenres,
-  setSelectedGenres,
-  genres,
-  setGenres,
-  type,
+const Genre = ({
+  media,
+  setFilterGenre,
   setPage,
+  genreTitle,
+  setGenreTitle,
+  setTreadingContent,
+  treadingContent,
+  handleRedirect,
 }) => {
-  const handleAdd = (genre) => {
-    setSelectedGenres([...selectedGenres, genre]);
-    setGenres(genres.filter((g) => g.id !== genre.id));
-    setPage(1);
-  };
-
-  const handleRemove = (genre) => {
-    setSelectedGenres(
-      selectedGenres.filter((selected) => selected.id !== genre.id)
-    );
-    setGenres([...genres, genre]);
-    setPage(1);
-  };
+  const [getGenre, setGetGenre] = useState([]);
+  const navigate = useHistory();
+  // let location = useLocation();
 
   const fetchGenres = async () => {
-    const { data } = await axios.get(
-      `https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
-    );
-    setGenres(data.genres);
+    try {
+      if (media) {
+        const { data } = await axios.get(
+          `https://api.themoviedb.org/3/genre/${media}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+        );
+        // eslint-disable-next-line
+        setGetGenre(data.genres);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleFilter = (genre) => {
+    setFilterGenre([genre]);
+    setPage(1);
+    setGenreTitle(genre);
+    if (media === "tv") {
+      navigate.push(`/${media}=series${genre.id}-Category`);
+    } else if (media === "movie") {
+      navigate.push(`/${media}-${genre.name}-Category`);
+    } else if (!media) {
+      navigate.push("/all-movies");
+    }
   };
 
   useEffect(() => {
     fetchGenres();
-
     return () => {
-      setGenres({}); // unmounting
+      setGetGenre();
     };
     // eslint-disable-next-line
   }, []);
 
   return (
-    <div style={{ padding: "6px 0" }}>
-      {selectedGenres.map((genre) => (
-        <Chip
-          style={{ margin: 2 }}
-          label={genre.name}
-          key={genre.id}
-          color="primary"
-          clickable
-          size="small"
-          onDelete={() => handleRemove(genre)}
-        />
-      ))}
-      {genres.map((genre) => (
-        <Chip
-          style={{ margin: 2 }}
-          label={genre.name}
-          key={genre.id}
-          clickable
-          size="small"
-          onClick={() => handleAdd(genre)}
-        />
-      ))}
-    </div>
+    <>
+      <div className="dropdown" style={{ position: "relative " }}>
+        <Link
+          className="btn btn-secondary dropdown-toggle mybtn"
+          to="#"
+          role="button"
+          id="dropdownMenuLink"
+          data-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Filter By:{" "}
+        </Link>
+
+        <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+          <div className="title__genre">Categories</div>
+
+          <div className="category__content">
+            <p className="dropdown-item3" onClick={handleFilter}>
+              all {media === "movie" ? "Movies" : "Tv series"}
+            </p>
+            {getGenre &&
+              getGenre.map((genre) => (
+                <p
+                  key={genre.id}
+                  onClick={() => handleFilter(genre)}
+                  className="dropdown-item2"
+                >
+                  {genre.name}
+                </p>
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
-export default Genres;
+export default Genre;
